d3dfb81f1bcb2d8274f695c5426a1229d08f4d36
Hamza Bouzid <hamzadeveloper1@gmail.com>
2021-12-11 13:49:49 +0100

    completato

---
+++ main.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("content");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+    container.innerHTML += container.innerHTML = `
+    <div class="box fizz-buzz">FizzBuzz</div> 
+    `;
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+    container.innerHTML += container.innerHTML = `
+   <div class="box fizz">Fizz</div> 
+   `;
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+    container.innerHTML += container.innerHTML = `
+    <div class="box buzz">Buzz</div> 
+    `;
+  } else {
+    console.log(i);
+    container.innerHTML += container.innerHTML = `
+   <div class="box">${i}</div> 
+   `;
+  }
+}
